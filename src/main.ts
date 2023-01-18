d708f386ec0b297e55cd6129b6f8c4e7e7e0b4cb
Dustin Schipper <dschipper@cuningham.com>
2023-01-18 02:32:18 -0600

    added vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,19 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "vuetify/styles";
+import { createVuetify } from "vuetify/lib/framework.mjs";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({ components, directives });
+
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
