0915edf6e78194e8788e04eda8ae52874b97f1e0
Dustin Schipper <dschipper@cuningham.com>
2023-01-19 00:22:15 -0600

    added tooltips to navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,33 @@ const router = useRouter();
   <v-layout>
     <v-app-bar color=""></v-app-bar>
     <v-navigation-drawer rail color="" permanent>
-      <v-list>
+      <v-list class="mt-0 pt-0">
         <v-divider />
-        <v-list-item
-          prepend-icon="mdi-table-multiple"
-          value="build-program"
-          :active="route.name == 'build-program'"
-          @click="router.push('/')"
-        />
+        <div>
+          <v-list-item
+            prepend-icon="mdi-table-multiple"
+            value="build-program"
+            :active="route.name == 'build-program'"
+            @click="router.push('/')"
+          >
+          </v-list-item>
+          <v-tooltip activator="parent" location="end">
+            Create Program
+          </v-tooltip>
+        </div>
         <v-divider />
-        <v-list-item
-          prepend-icon="mdi-circle"
-          value="adjacency"
-          :active="route.name == 'adjacency'"
-          @click="router.push('adjacency')"
-        />
+        <div>
+          <v-list-item
+            prepend-icon="mdi-circle"
+            value="adjacency"
+            :active="route.name == 'adjacency'"
+            @click="router.push('adjacency')"
+          >
+          </v-list-item>
+          <v-tooltip activator="parent" location="end">
+            Manage Adjacencies
+          </v-tooltip>
+        </div>
         <v-divider />
       </v-list>
 
@@ -31,6 +43,7 @@ const router = useRouter();
         <div class="pa-2">
           <v-btn variant="plain" block>
             <v-icon size="x-large">mdi-cog</v-icon>
+            <v-tooltip activator="parent" location="end"> Settings </v-tooltip>
           </v-btn>
         </div>
       </template>
