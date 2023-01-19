82ca213347ed30f4c5966112d3423a26bbf54b5a
Dustin Schipper <dschipper@cuningham.com>
2023-01-19 00:33:57 -0600

    separated navigation drawer into it's own component

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,12 @@
 <script setup lang="ts">
-import { RouterView, useRoute, useRouter } from "vue-router";
-
-const route = useRoute();
-const router = useRouter();
+import { RouterView } from "vue-router";
+import NavigationDrawer from "./components/NavigationDrawer.vue";
 </script>
 
 <template>
   <v-layout>
     <v-app-bar color=""></v-app-bar>
-    <v-navigation-drawer rail color="" permanent>
-      <v-list class="mt-0 pt-0">
-        <v-divider />
-        <div>
-          <v-list-item
-            prepend-icon="mdi-table-multiple"
-            value="build-program"
-            :active="route.name == 'build-program'"
-            @click="router.push('/')"
-          >
-          </v-list-item>
-          <v-tooltip activator="parent" location="end">
-            Create Program
-          </v-tooltip>
-        </div>
-        <v-divider />
-        <div>
-          <v-list-item
-            prepend-icon="mdi-circle"
-            value="adjacency"
-            :active="route.name == 'adjacency'"
-            @click="router.push('adjacency')"
-          >
-          </v-list-item>
-          <v-tooltip activator="parent" location="end">
-            Manage Adjacencies
-          </v-tooltip>
-        </div>
-        <v-divider />
-      </v-list>
-
-      <template v-slot:append>
-        <div class="pa-2">
-          <v-btn variant="plain" block>
-            <v-icon size="x-large">mdi-cog</v-icon>
-            <v-tooltip activator="parent" location="end"> Settings </v-tooltip>
-          </v-btn>
-        </div>
-      </template>
-    </v-navigation-drawer>
+    <NavigationDrawer />
     <v-main>
       <RouterView />
     </v-main>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { RouterView, useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+</script>
+
+<template>
+  <v-navigation-drawer rail color="" permanent>
+    <v-list class="mt-0 pt-0">
+      <v-divider />
+      <div>
+        <v-list-item
+          prepend-icon="mdi-table-multiple"
+          value="build-program"
+          :active="route.name == 'build-program'"
+          @click="router.push('/')"
+        >
+        </v-list-item>
+        <v-tooltip activator="parent" location="end">
+          Create Program
+        </v-tooltip>
+      </div>
+      <v-divider />
+      <div>
+        <v-list-item
+          prepend-icon="mdi-circle"
+          value="adjacency"
+          :active="route.name == 'adjacency'"
+          @click="router.push('adjacency')"
+        >
+        </v-list-item>
+        <v-tooltip activator="parent" location="end">
+          Manage Adjacencies
+        </v-tooltip>
+      </div>
+      <v-divider />
+    </v-list>
+
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn variant="plain" block>
+          <v-icon size="x-large">mdi-cog</v-icon>
+          <v-tooltip activator="parent" location="end"> Settings </v-tooltip>
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
